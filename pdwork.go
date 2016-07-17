4f89f1edbe5f955863222754810d77c541f686f9
Kentaro KAZUHAMA <kazken3@gmail.com>
2016-07-18 00:04:28 +0900

    update

--- pdwork.go
+++ pdwork.go
@@ -11,12 +11,12 @@ import (
 var outformat = map[string]string{"odt": "odt", "rtf": "rtf", "json": "json", "html": "html", "html5": "html5", "md": "markdown"}
 
 func PandocExec(inputfile, ftype string) string {
-	var in string = "testoutput/" + inputfile
+	var in string = "$HOME/testoutput/" + inputfile
 	i, j := strings.LastIndex(inputfile, "/")+1, strings.LastIndex(inputfile, path.Ext(inputfile))
 	name := inputfile[i:j]
 
 	var outfile string = name + "." + ftype
-	var out string = "testoutput/" + name + "." + ftype
+	var out string = "$HOME/testoutput/" + name + "." + ftype
 
 	cmd := exec.Command("pandoc", in, "-t", outformat[ftype], "-s", "-o", out)
 	err := cmd.Run()
